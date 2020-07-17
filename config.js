a63f8927261fff632bf8e843582e37bd5d0671c0
lexArk22 <63560878+lexArk22@users.noreply.github.com>
2020-07-17 20:09:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.token = "NzA1ODQ2MTQzODE2MzAyNTk0.XrR0Fw.VbUwO8cYf9g0f5t-YW8O0A1BURA";
+exportations . token  =  "" ;
 exports.prefix = "y!";
 exports.owner = [],
-exports.youtube_API_key = "AIzaSyClyk7IzIIgm2-FUl5G69E8MHr7e4yZaxw"
+exportations . youtube_API_key  =  ""
